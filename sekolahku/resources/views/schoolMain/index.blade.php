2d5d9f87990d898d8a30f4828c8f53b6d6daf1fb
stevanstev <stevanussteven7@gmail.com>
2019-03-02 14:35:59 +0700

    Update index.blade.php

--- sekolahku/resources/views/schoolMain/index.blade.php
+++ sekolahku/resources/views/schoolMain/index.blade.php
@@ -1,7 +1,7 @@
 @extends('applicationMain/index')
 
 @section('optionalHeader')
-    <link rel="stylesheet" href="https://{{ asset('schoolassets/css') }}/{{ $data->css }}">
+    <link rel="stylesheet" href="{{ secure_asset('schoolassets/css') }}/{{ $data->css }}">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 @endsection
@@ -15,7 +15,7 @@
         <div class="row">
             <div class="col-md-1"></div>
             <div class="col-md-2">
-                <img src="https://{{ asset('schoolassets/images') }}/{{ $data->schoolLogo }}" class="img-responsive" id="schoolLogo">
+                <img src="{{ secure_asset('schoolassets/images') }}/{{ $data->schoolLogo }}" class="img-responsive" id="schoolLogo">
             </div>
             <div class="col-md-6" id="slogan">
                 <h4><b>{{ $data->schoolName }}</b></h4>
@@ -29,7 +29,7 @@
                 </select>
             </div>
             <div class="col-md-1">
-                <img src="https://{{ asset('schoolassets/images') }}/{{ $data->optImage }}" class="img-responsive" id="ribbonLogo">
+                <img src="{{ secure_asset('schoolassets/images') }}/{{ $data->optImage }}" class="img-responsive" id="ribbonLogo">
             </div>
         </div>
     </div>
