27cfdcb58ab8455e907e74a5bb1d496dad036cea
stevanstev <stevanussteven7@gmail.com>
2019-03-02 17:17:50 +0700

    Update web.php

--- sekolahku/routes/web.php
+++ sekolahku/routes/web.php
@@ -11,6 +11,10 @@
 |
 */
 
+if (env('APP_ENV') === 'production') {
+    URL::forceSchema('https');
+}
+
 Route::group(['middleware'=>'guest'],function(){
     Route::get('/',['uses'=>'SchoolController@index']);
     Route::post('/changelang',['uses'=>'SchoolController@changelang']);
