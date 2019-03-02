69fd8088f8fa8d9af9fe1bc962629d1f1c01863f
stevanstev <stevanussteven7@gmail.com>
2019-03-02 14:32:40 +0700

    Update index.blade.php

--- sekolahku/resources/views/schoolMain/index.blade.php
+++ sekolahku/resources/views/schoolMain/index.blade.php
@@ -1,7 +1,7 @@
 @extends('applicationMain/index')
 
 @section('optionalHeader')
-    <link rel="stylesheet" href="{{ asset('schoolassets/css') }}/{{ $data->css }}">
+    <link rel="stylesheet" href="https://{{ asset('schoolassets/css') }}/{{ $data->css }}">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 @endsection
