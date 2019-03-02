9c1106df336a35ef3d23fa0791a63583b411955c
stevanstev <stevanussteven7@gmail.com>
2019-03-02 14:29:15 +0700

    Update header.blade.php

--- sekolahku/resources/views/template/header.blade.php
+++ sekolahku/resources/views/template/header.blade.php
@@ -1,8 +1,4 @@
-<script 
-    src="https://code.jquery.com/jquery-3.3.1.min.js" 
-    integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
-    crossorigin="anonymous">
-</script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script 
     src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js">
 </script>    
